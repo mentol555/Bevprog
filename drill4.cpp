01f0dc971858fdd1dca906ba17d06e3e4e392a58
mentol555 <55355512+mentol555@users.noreply.github.com>
2019-10-05 00:48:07 +0200

    Update drill4.cpp

--- drill4.cpp
+++ drill4.cpp
@@ -1,52 +1,44 @@
 
-#include <iostream>
-#include <string>
-#include <vector>
 #include "std_lib_facilities.h"
+#include <string>
 using namespace std;
-  double alakit(string unit)
+vector <double> szamok;
+double alakit(double &x,string unit)
 {
-    if(unit=="m")
-  return 1;
-    if(unit=="cm")
-  return 0.01;
-    if(unit=="in")
-  return 0.0254;
-    if(unit=="ft")
-  return 0.3048;
+	if(unit=="m"){x=x*1;szamok.push_back(x);}
+	if(unit=="cm"){x=x/100.0; szamok.push_back(x);}
+	if(unit=="in"){x=x/39.3701; szamok.push_back(x);}
+	if(unit=="ft"){x=(x*12)/39.3701; szamok.push_back(x);}
 }
-  double val[100];
-    int main()
+int main()
 {
+double x,sum=0,mini=10000.0,maxi=-10000.0;
+int i=0;
+string unit;
+cout<<"Hello! Enter double type numbers and their units, if you want to end it enter |"<<endl;
+	while(cin>>x)
+	{	
+		++i;
+		cin>>unit;
+		if(unit=="m" || unit=="cm" || unit=="in" || unit=="ft")		
+		{
+		cout<<"entered number:"<<x<<endl;	
+		alakit(x,unit);
+		sum+=x;
+		if(x>maxi)
+			{cout<<"Largest number so far:"<<x<<"(in m)"<<endl;maxi=x;}
+		if(x<mini)
+			{cout<<"Smallest number so far:"<<x<<"(in m)"<<endl;mini=x;}
+		x=0;
+		}
+		else cout<<endl<<"Unknown unit!";
+	}
+	sort(szamok);
+	cout<<endl<<"Number of numbers:"<<i;
+	cout<<endl<<"Sum of numbers:"<<sum;
+	cout<<endl<<"List of numbers:";
+	for(int j=0;j<szamok.size();++j)
+	cout<<szamok[j]<<" ";
 
-  string unit;
-  double a,small,large,sum=0;
-  int num=1;
-  cin>>a;
-  cin>>unit;
-  small=large=a;
-  cout<<a<<" the smallest so far\n";
-  cout<<a<<" the largest so far\n";
-  sum=sum+a*alakit(unit);
-  val[num]=a*alakit(unit);
-  while(cin>>a){cin>>unit;cout<<"\nbentvan\n";
-  if(unit=="cm" || unit=="m" || unit=="in" || unit=="ft")
-  {sum=sum+a*alakit(unit);
-  num++;
-  val[num]=a*alakit(unit);
-  if(a>large)
-  {large=a;
-  cout<<a<<" the largest so far\n";}
-  if(a<small)
-  {small=a;
-  cout<<a<<" the smallest so far\n";}
-
-  cout<<endl;
-  }
-  else cout<<"Rejected\n";
-  }
-  sort(val);
-  cout<<sum<<" "<<num<<endl;
-  for(int i=1;i<=num;i++)
-  cout<<val[i]<<" ";
-  }
+return 0;
+}
